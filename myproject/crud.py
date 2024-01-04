6767f0ab0b616e0601ec483a838f8f35f2634f30
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2024-01-04 12:48:27 +0100

    initial changes from project 1

--- myproject/crud.py
+++ myproject/crud.py
@@ -4,12 +4,12 @@ import models
 import schemas
 
 
-def get_name(db: Session, user_name: str):
-    return db.query(models.Star_wars).filter(models.Star_wars.name == user_name).first()
+def get_user(db: Session, user_name: str):
+    return db.query(models.user).filter(models.user.name == user_name).first()
 
 
-def delete_character_by_name(db: Session, user_name: str):
-    db_user = db.query(models.Star_wars).filter(models.Star_wars.name == user_name).first()
+def delete_user_by_name(db: Session, user_name: str):
+    db_user = db.query(models.user).filter(models.user.name == user_name).first()
     if db_user:
         db.delete(db_user)
         db.commit()
@@ -17,26 +17,12 @@ def delete_character_by_name(db: Session, user_name: str):
     return None
 
 
-def get_faction_by_name(db: Session, faction_name: str):
-    return db.query(models.Faction).filter(models.Faction.name == faction_name).first()
-
-
-def get_faction(db: Session, faction: int, limit: int = 10):
-    return db.query(models.Star_wars).filter(models.Star_wars.faction_id == faction).limit(limit).all()
-
-
-def create_character(db: Session, user: schemas.Star_wars_create):
-    db_star_Wars = models.Star_wars(**user.dict())
+def create_user(db: Session, user: schemas.user_create):
+    db_star_Wars = models.user(**user.dict())
     db.add(db_star_Wars)
     db.commit()
     db.refresh(db_star_Wars)
     return db_star_Wars
 
 
-def create_faction(db: Session, faction: schemas.FactionCreate):
-    db_faction = models.Faction(**faction.dict())
-    db.add(db_faction)
-    db.commit()
-    db.refresh(db_faction)
-    return db_faction
 
--- myproject/models.py
+++ myproject/models.py
@@ -1,23 +1,14 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 
 from database import Base
 
 
-class Star_wars(Base):
+class user(Base):
     __tablename__ = "star_wars"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    faction_id = Column(Integer, ForeignKey("faction.id"))
-    lightsaber_Wielder = Column(Boolean, default=True)
-    age = Column(Integer)
-    species = Column(String)
+    fullName = Column(String, unique=True, index=True)
+    userName = Column(String, unique=True, index=True)
+    password = Column(String, unique=True, index=True)
+
     addition = Column(String)
-
-
-class Faction(Base):
-    __tablename__ = "faction"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-
